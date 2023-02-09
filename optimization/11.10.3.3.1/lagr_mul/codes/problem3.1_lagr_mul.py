5e8c12777c50cdc181203aed4ffe971e9b04e430
satheeshsimha <satheeshsk@hotmail.com>
2023-02-09 15:29:16 +0530

    changes

--- optimization/11.10.3.3.1/lagr_mul/codes/problem3.1_lagr_mul.py
+++ optimization/11.10.3.3.1/lagr_mul/codes/problem3.1_lagr_mul.py
@@ -54,8 +54,8 @@ import shlex
 #  return line_intersect(n,cn,m,cm)
 
 #Input parameters and other points
-iters = 1000
-alpha = 0.01
+iters = 1000 
+alpha = 0.05
 x_old = np.array([[0],[8/math.sqrt(3)]])
 O = np.array([[0],[0]])
 T = np.array([[-8],[0]])
@@ -63,25 +63,29 @@ S = np.array([[4],[12/math.sqrt(3)]])
 eps = 1e-6
 n = np.array([[1],[-math.sqrt(3)]])
 m = np.array([[1],[1/math.sqrt(3)]])
-c = -8 
+c = -8
 i = 0
 L = line_gen(S,T)
 plt.plot(L[0],L[1])
-
+dot_p = 1 
 #Function to plot the circle
 def plot_circ(p):
     C = circ_gen(O.T,np.linalg.norm(O-p))
     plt.plot(C[0],C[1])
 
-while(i<iters):
-    delta = alpha*m*(m.T@(O-x_old))
+#Iterate until the dot product is zero or the iterations are reached
+while(i<iters and dot_p != 0):
+    dot_p = m.T@(O-x_old)
+    delta = alpha*m*dot_p
     #Every 25 iterations, we plot the results
-    if i%40 == 0: 
+    if i%5 == 0: 
         plot_circ(x_old)
     i=i+1
     x_old = x_old + delta
-#Final circle
-#plot_circ(x_old)
+    #Standard round() is not giving right output. Hence this workaround was used
+    dec, integ = np.modf(dot_p)
+    dot_p = np.abs(integ+np.round(dec,5))
+
 M = line_gen(O,x_old)
 plt.plot(M[0],M[1])
 plt.plot(O[0],O[1],'k.')
